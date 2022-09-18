db846d9e8723d862aa73642f656458efa59c2a2e
MINJU-KIMmm <0112km@ewhain.net>
2022-09-18 16:57:56 +0900

    03. 영속성 관리 - 준영속

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -16,33 +16,14 @@ public class JpaMain {
         tx.begin();
 
         try{
-            //비영속
-/*
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
-*/
-
             //영속
-/*
-            Member findMember1 = em.find(Member.class, 101L);
-            Member findMember2 = em.find(Member.class, 101L); //영속성 컨텍스트에 있는 1차 캐시에 있는 걸 가져온다.
-
-            System.out.println("result = " + (findMember1 == findMember2)); //영속 엔티티의 동일성 보장 -> true (1차 캐시 때문. 1차 캐시로 반복가능한 읽기)
-            tx.commit();
-*/
-
-            /*Member member1 = new Member(212L, "A");
-            Member member2 = new Member(121L, "B");
-
-            em.persist(member1);
-            em.persist(member2); //이때마다 다 쿼리를 날리면 최적화할 수 있는 여지가 없다 -> 한번에 보내지 않음. hibernate.jdbc.batch_size = 이 사이즈만큼 모았다가 데이터베이스에 쿼리 보냄(버퍼링 기능)
-
-            System.out.println("=======");
-*/
-
             Member member = em.find(Member.class, 150L);
-            member.setName("zzzz"); // dirty checking(변경 감지). 이후에 em.persist(member) 해주는 것 x
+            member.setName("aaaa");
+
+            em.detach(member); //준영속상태됨 -> JPA가 관리 x
+            //-> select query만 나가고 update는 x
+            //em.clear() : 영속성 컨텍스트 통째로 초기화
+            //em.close() : 영속성 컨텍스트 종료
 
             System.out.println("------");
             tx.commit();
